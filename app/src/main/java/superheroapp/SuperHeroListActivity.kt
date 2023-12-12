f5e447f94cc4929bc2f6abc4b8deba59a1035b8d
Javier García <javigpdeveloper@gmail.com>
2023-12-12 17:58:24 +0100

    Init Superhero

--- app/src/main/java/superheroapp/SuperHeroListActivity.kt
+++ app/src/main/java/superheroapp/SuperHeroListActivity.kt
@@ -1,4 +1,12 @@
 package superheroapp
 
-class SuperHeroListActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.javier.cursokotlin.R
+
+class SuperHeroListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_super_hero_list)
+    }
 }
